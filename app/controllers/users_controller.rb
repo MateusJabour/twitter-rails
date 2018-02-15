f2d1c282c9c3ccddfa05a299535d565cce97f474
Mateus Jabour <mjabourbp@gmail.com>
2018-02-15 16:18:57 -0500

    Adds route that return list of users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   skip_before_action :authorize_request, only: :create
 
+  def index
+    @users = User.all.select(:id, :first_name, :last_name, :username, :email)
+    json_response(@users)
+  end
+
   def create
     user = User.create!(user_params)
     auth_token = AuthenticateUser.new(user.email, user.password).call
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   post 'tweets', to: 'tweets#create'
 
   post 'auth/login', to: 'authentication#authenticate'
+
+  get 'users', to: 'users#index'
   post 'signup', to: 'users#create'
 end
